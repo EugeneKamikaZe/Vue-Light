b93c037a74a89ee7f3df69df99ee5f3d83e77b66
Eugene <12qweR10y>
2021-10-03 20:51:54 +0300

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import LightBulb from '@/components/LightBulb';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: LightBulb,
+      children: [
+        {
+          path: '/red',
+          name: 'Red',
+          component: LightBulb,
+        },
+        {
+          path: '/yellow',
+          name: 'Yellow',
+          component: LightBulb,
+        },
+        {
+          path: '/green',
+          name: 'Green',
+          component: LightBulb,
+        },
+      ],
+    },
+  ],
+});
